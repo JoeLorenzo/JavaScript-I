47e380cd8d0d7ba0249cd2d8283984b006d0dc07
JoeLorenzo <Joselmedrano@gmail.com>
2019-06-23 01:08:16 -0500

    a

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -54,6 +54,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
 
 
+
 // Example for loop:
 
 // arr = [1,2,3,4];
@@ -66,11 +67,12 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
 
 
+
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
 console.log();
-
+g
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
